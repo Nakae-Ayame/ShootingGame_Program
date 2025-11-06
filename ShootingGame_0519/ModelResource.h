b8e1125eaaf019b57941043777fe567e365e342e
SasakurePanda <Iris.11031221@docomo.ne.jp>
2025-11-06 13:24:56 +0900

    モデルを使いまわせるようにする仕組み用の.cppと.hを追加
    追加したけどそこよりも先にやるべきところがあるので一旦おいておく

---
+++ ShootingGame_0519/ModelResource.h
@@ -0,0 +1,16 @@
+//#pragma once
+//#include <vector>
+//#include <string>
+//#include <wrl/client.h>
+//#include <memory>
+//#include <d3d11.h>
+//#include "commontypes.h"
+//
+//using Microsoft::WRL::ComPtr;
+//
+//struct ModelMeshInfo
+//{
+//
+//
+//
+//};
